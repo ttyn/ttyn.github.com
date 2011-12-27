27879d28d51743f37441886c04a9cd7d8a308b89
Andy X <andyxiang.work@gmail.com>
2011-12-27 16:52:01 +0800

    [proxy] 1. stop using autossh check port because it wont work anyway 2. add the tools/quick_check_proxy.py to check the validity of the proxy TODO: 1. tools/quick_check_proxy.py check against the pac 2. validate the pac file

--- working_stuffs/set_proxy/client/start_proxy.sh
+++ working_stuffs/set_proxy/client/start_proxy.sh
@@ -7,11 +7,11 @@ source ~/.bash_profile
 : ${proxy_host:="first_proxy_host"}
 : ${local_proxy_server_ip:="127.0.0.1"}
 SSH_CMD="autossh -M 0"
-SSH_CMD_MON="autossh -M "
+SSH_CMD_MON="autossh -M 11080"
 
 case $command in
         start)  echo "#----`date '+%Y-%m-%d'` --- starting the proxy"
-                ${SSH_CMD_MON} 11080 -qTfnN -D ${local_proxy_server_ip}:${port_number} ${proxy_host}
+                ${SSH_CMD} -qTfnN -D ${local_proxy_server_ip}:${port_number} ${proxy_host}
         ;;
         stop)  echo "#----`date '+%Y-%m-%d'` --- use SIGTERM to stop the proxy"
                kill -s SIGTERM `pidof autossh`
@@ -20,7 +20,7 @@ case $command in
                  pidof autossh
         ;;
         test)  echo "#----`date '+%Y-%m-%d'` --- test proxy connection"
-                 ${SSH_CMD_MON} 11080 -vvvTN -D ${local_proxy_server_ip}:${port_number} ${proxy_host}
+                 ${SSH_CMD} -vvvTN -D ${local_proxy_server_ip}:${port_number} ${proxy_host}
                  #TODO add real test to the proxy
         ;;
         *) echo "please use ./start_proxy.sh [start|stop] to manipulate tunnel" && exit 1
