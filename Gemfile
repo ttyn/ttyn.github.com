39b3d6d635c1bcf2edbc948adad5ed437a19865b
Andy X <andyxiang.work@gmail.com>
2012-02-09 12:23:28 +0800

    [gh-page] use master for nanoc code

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source :gemcutter
+# local webserver
+gem 'adsf'
+gem 'mime-types'
+gem 'rack'
+# syntax colorizing filter
+gem 'coderay'
+gem 'nokogiri'
+# textile filter
+gem 'RedCloth'
+# xml sitemap
+gem 'builder'
+# nanoc
+gem 'nanoc'
+
---
+++ sa_homework/lib/helpers_.rb
@@ -0,0 +1,11 @@
+# file is called includes_.rb and not includes.rb
+# to ensure this file is loaded last (after the custom
+# Portfolio helper is loaded) to prevent errors
+# (files are loaded alphabetically)
+include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::LinkTo
+include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::Tagging
+include Nanoc3::Helpers::Text
+include Nanoc3::Helpers::XMLSitemap
+#include Portfolio
