fb6268fef1c904c46f1e194fd671477d8bfc111e
Andy X <andy@ttyn.me>
2013-10-20 23:57:08 -0700

    update the format and the vendor link

--- config.rb
+++ config.rb
@@ -12,7 +12,10 @@
 # constant Gollum::Page::FORMAT_NAMES not defined (NameError)
 Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
 # limit to one format
-Gollum::Page::FORMAT_NAMES = { :markdown  => "Markdown" }
+Gollum::Page::FORMAT_NAMES = { 
+        :markdown  => "Markdown",
+        :rest      => "reStructuredText"
+        }
 
 =begin
 Valid formats are:
