845fa207f5e3155475c3a5e94ffe511df61e994b
Andy X <andy@ttyn.me>
2013-10-21 19:54:23 -0700

    update a bit of detail

--- config.rb
+++ config.rb
@@ -13,8 +13,7 @@
 Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
 # limit to one format
 Gollum::Page::FORMAT_NAMES = { 
-        :markdown  => "Markdown",
-        :rest      => "reStructuredText"
+        :markdown  => "Markdown"
         }
 
 =begin
